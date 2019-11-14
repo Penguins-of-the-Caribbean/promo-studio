cac9c4a6ff5fd3377b6fa3801e15515ddc946317
Pablo Vazquez <pvazquez@vandldigital.com>
2019-11-14 11:59:43 -0500

    basics

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './navbar.css';
+
+export default function index() {
+    return (
+        <div className="ge_promo-studio-navbar">
+            <h1>Promo <img src='/imgs/logo/bullhorn.png' alt='Logo'></img> Studio</h1>
+        </div>
+    )
+}
